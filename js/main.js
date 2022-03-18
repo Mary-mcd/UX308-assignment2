2a9db70b48697f1670789939a8692dcb5751cf33
Mary-mcd <marymcdonaldd@gmail.com>
2022-03-18 19:51:13 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -16,4 +16,29 @@ window.onload = () => {
   }
 
   navigator.serviceWorker.register('/service-worker.js')
+
+  self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+  
+  /* Serve cached content always */
+  self.addEventListener('fetch', function(e) {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
+  
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').catch((registrationError) => {
+        console.error('ServiceWorker registration failed: ', registrationError)
+      })
+    })
+  }
   
