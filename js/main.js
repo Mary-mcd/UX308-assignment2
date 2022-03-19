17386d1f7a157d8ce0388f03d1bac82cf8b04921
Mary-mcd <marymcdonaldd@gmail.com>
2022-03-18 21:54:04 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
  if ('serviceWorker' in navigator){
   navigator.serviceWorker.register('/sw.js')
   .then ((reg) => console.log('service worker regestered', reg))
-  .catch ((err) => console.log('service worker not regestered', err))}
+  .catch ((err) => console.log('service worker not regestered', err))
+}
